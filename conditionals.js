baef6434b36431e7d9129a69df18e57100fa6554
Taha Mulla <tahamulla@hotmail.com>
2023-05-19 17:45:20 -0400

    Use conditionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,29 @@
+const raining = false;
+
+const cold = false;
+
+const temperature = 18;
+
+if (raining) {
+console.log("Dont forget your umbrella!");
+}
+
+if (cold) {
+console.log("Make sure you pick out a scarf!");
+}
+
+if (temperature < 0) {
+
+console.log("Make sure you pick out a scarf!");
+
+} else if (temperature < 15) {
+
+console.log("short sleeves won't cut it!");
+
+} else {
+
+console.log("Short sleeves are fine.");
+
+}
+
+console.log("Now you're ready to go outside!");
