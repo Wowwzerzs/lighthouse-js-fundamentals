ae1389cac88b58b273c448fc674e6a9c9d144e3c
Taha Mulla <tahamulla@hotmail.com>
2023-05-25 16:57:29 -0400

    Declaring and Calling Functions

---
+++ hello.js
@@ -0,0 +1,13 @@
+const sayHello = function () { /* declared a function named (sayHello) */
+        console.log("hello, world"); // prints string to the console
+}
+sayHello(); //calls or invokes the function
+
+// improved code below
+
+const sayHello = function (name) { /* declared a function(sayHello) with the parameter (name) */
+   console.log("Hello, " + name); // Prints to the console
+}
+sayHello("Josh");  //calls or invokes the function
+sayHello("Angela");
+sayHello("Peter");
