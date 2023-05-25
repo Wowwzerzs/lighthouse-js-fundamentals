885047bc5b3e34bd0f90173c46534fc95574fb70
Taha Mulla <tahamulla@hotmail.com>
2023-05-25 17:18:30 -0400

    Calling return instead of console.log

---
+++ even.js
@@ -0,0 +1,6 @@
+const isEven = function (num) { /* Declared a function (isEven) with the parameter (num)*/
+   return num % 2 === 0; //return statment 
+}
+
+console.log(isEven(10)); // invoked the function (isEven)
+console.log(isEven(11));
