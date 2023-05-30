f07969b3d6a40a8dc23a44e856319adb4eeb8d64
Taha Mulla <tahamulla@hotmail.com>
2023-05-30 18:16:30 -0400

    Array with while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,18 @@
+// Array with for loop
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+// Array with while loop
+
+/*
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+*/
